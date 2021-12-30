c5332c205c4f31ab89e59dec41c0eda5319b1fd1
Stanley Pan <imoment.developer@gmail.com>
2021-12-30 14:57:54 -0500

    Initial Commit

---
+++ Tinder Build/Tinder_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_BuildApp.swift
+//  Tinder Build
+//
+//  Created by Stanley Pan on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
